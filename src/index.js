9448c969bb6bc001033c604a0b23620e6bb28a8f
dmkolosov <maker.bug.00@inbox.ru>
2020-10-25 09:59:52 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -39,7 +39,69 @@ const MORSE_TABLE = {
 
 function decode(expr) {
 
+    let binaryArray=[] 
+    for (let index = 0; index < expr.length/10; index++) {
+        
+    
+        if (index==0){
+            console.log(expr.slice(index,index+10),index)
+            binaryArray.push( expr.slice(index,index+10))}
+        
+    else if(index*10+10<=expr.length){
+        console.log(expr.slice(index*10,index*10+10),"---",index,"x,")
+        binaryArray.push( expr.slice(index*10,index*10+10))}
+    
+    
+        
+        
+    }
+    
+    let morseArray=[]
+    binaryArray.forEach(x=>{
+        if(x=="**********"){
+            
+            morseArray.push("space")
+        }
+        else{
+            morseArray.push(decodeToMorse(x))
+        }
+    
+        
+    })
+    
+    console.log("binaryArray",binaryArray)
+    let finish=[]
+    morseArray.forEach(x=>{
+    
+        if(x=="space"){
+            finish.push(" ")
+        }
+        else{
+            finish.push(MORSE_TABLE[x])
+        }
+        
+    
+    })
+    console.log(finish)
+        return finish.join("")
+    
+        
+        function decodeToMorse(x){
 
+            let code=[]
+        
+            for (let index = 0; index < x.length; index+=2) {
+               let symbol=x[index]+x[index+1]
+                if(symbol=="10"){
+                    code.push(".")
+                }
+                if(symbol=="11"){
+                    code.push("-")
+                }
+            }
+        
+            return code.join("")
+        }
 }
 
 module.exports = {
